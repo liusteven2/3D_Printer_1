3883d56ff8847444c35bcd64985e9fbf4a916223
liust <liusteven_2@tamu.edu>
2022-11-01 13:31:44 -0500

    Communication to firebase database write and read should work but it is not

--- app/src/main/java/com/example/a3d_printer_1/Library.kt
+++ app/src/main/java/com/example/a3d_printer_1/Library.kt
@@ -1,6 +1,7 @@
 package com.example.a3d_printer_1
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,16 +16,14 @@ import com.example.a3d_printer_1.databinding.FragmentLibraryBinding
 import com.google.firebase.database.*
 
 class Library : Fragment() {
-
-    //creating recyclerview and receiving information from firebase
-    private lateinit var dbref : DatabaseReference
-    private lateinit var userRecyclerView: RecyclerView
-    private lateinit var userArrayList : ArrayList<User>
+//
+//    //creating recyclerview and receiving information from firebase
+//    private lateinit var dbref : DatabaseReference
+//    private lateinit var userRecyclerView: RecyclerView
+//    private lateinit var userArrayList : ArrayList<User>
 
     //for sending information to firebase database
-    private lateinit var binding : ActivityMainBinding
     private lateinit var database : DatabaseReference
-//    private var fragmentLibraryBinding : FragmentLibraryBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,57 +47,58 @@ class Library : Fragment() {
 //            showToast("hello binding!")
 //        }
 //        return view
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-//        val btn : Button = view.findViewById(R.id.submitBtn)
-//        btn.setOnClickListener{
-//            val editText : EditText = view.findViewById(R.id.ediText)
-//            val input = editText.text.toString()
-////            database = FirebaseDatabase.getInstance().getReference("Users")
-////            val User = User(input)
-////            database.child(input).setValue(User).addOnSuccessListener {
-////                Toast.makeText(activity, "Successfully Saved",Toast.LENGTH_SHORT).show();
-////            }.addOnFailureListener {
-////                Toast.makeText(activity, "Failed Saved", Toast.LENGTH_SHORT).show();
-////            }
-//        }
+        val view = inflater.inflate(R.layout.fragment_library, container, false)
+        val btn : Button = view.findViewById(R.id.submitBtn)
+        btn.setOnClickListener{
+            val editText : EditText = view.findViewById(R.id.submitEdittext)
+            val input = editText.text.toString()
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(input)
+            database.child(input).setValue(User).addOnSuccessListener {
+                editText.text.clear()
+                Toast.makeText(activity, "Successfully Saved",Toast.LENGTH_SHORT).show();
+            }.addOnFailureListener {
+                Toast.makeText(activity, "Failed Saved", Toast.LENGTH_SHORT).show();
+            }
+        }
         return view
     }
 
 
-
+//
 //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //        super.onViewCreated(view, savedInstanceState)
 //
-////        //for receiving firebase data into recyclerview
-////        val layoutManager = LinearLayoutManager(context)
-////
-////        userRecyclerView = view.findViewById(R.id.recycler_view)
-////        userRecyclerView.layoutManager = layoutManager
-////        userRecyclerView.setHasFixedSize(true)
-////
-////        userArrayList = arrayListOf<User>()
-//////        getUserData()
+//        //for receiving firebase data into recyclerview
+//        val layoutManager = LinearLayoutManager(context)
+//
+//        userRecyclerView = view.findViewById(R.id.recycler_view)
+//        userRecyclerView.layoutManager = layoutManager
+//        userRecyclerView.setHasFixedSize(true)
+//
+//        userArrayList = arrayListOf<User>()
+//        getUserData()
 //    }
 
-
-    private fun getUserData() {
-        dbref = FirebaseDatabase.getInstance().getReference("Users")
-
-        dbref.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()){
-                    for (userSnapshot in snapshot.children){
-                        val user = userSnapshot.getValue(User::class.java)
-                        userArrayList.add(user!!) //note double exclamation points will throw an exception on null value
-                    }
-                    userRecyclerView.adapter = MyAdapter(userArrayList)
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
+//
+//    private fun getUserData() {
+//        dbref = FirebaseDatabase.getInstance().getReference("Users")
+//
+//        dbref.addValueEventListener(object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                if (snapshot.exists()){
+//                    for (userSnapshot in snapshot.children){
+//                        val user = userSnapshot.getValue(User::class.java)
+//                        userArrayList.add(user!!) //note double exclamation points will throw an exception on null value
+//                    }
+//                    userRecyclerView.adapter = MyAdapter(userArrayList)
+//                }
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
+//    }
 }
